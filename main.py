a6adec29b3829af045c2144c4648dc767d72b795
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-10-03 13:34:52 +0100

    Would be nice to use Forums, no?

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from pathlib import Path
+import logging
 import asyncio
 
 import pytomlpp as toml
 import pendulum
 import discord
 
+logging.basicConfig(
+  filename = "main.log",
+  format = "{asctime} {name} {levelname}: {message}",
+  style = "{",
+  level = logging.DEBUG,
+  encoding = "utf8"
+)
+logger = logging.getLogger("main")
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+  logging.info(f"We have logged in as {client.user}")
+  # await client.change_presence(activity = discord.CustomActivity("Blah"))
+  await client.change_presence(activity = discord.Game(name = "Selecting a quote!"))
 
 if __name__ == "__main__":
   client.run(Path("token.txt").read_text())
