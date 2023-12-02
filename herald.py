6466f9f7d34a961a333623767f5ffaf85f10b619
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-02 16:10:41 +0000

    Ruff by default

--- herald.py
+++ herald.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import logging
-import asyncio
 
 import pytomlpp as toml
-import pendulum
 import discord
 
 # this file is where we do the actual plumbing of Herald
@@ -15,25 +13,22 @@ assert isinstance(setup.servers[0], int)
 assert "token" in setup
 assert isinstance(setup.token, str)
 
-logging.basicConfig(
-  filename = "main.log",
-  format = "{asctime} {name} {levelname}: {message}",
-  style = "{",
-  level = logging.DEBUG,
-  encoding = "utf8"
-)
+logging.basicConfig(filename="main.log", format="{asctime} {name} {levelname}: {message}", style="{", level=logging.DEBUG, encoding="utf8")
 logger = logging.getLogger("herald")
 
 bot = discord.Bot()
 
+
 @bot.event
 async def on_ready():
   logging.info(f"We have logged in as {bot.user}")
-  await bot.change_presence(activity = discord.Game(name = "Heralding the Code!"))
+  await bot.change_presence(activity=discord.Game(name="Heralding the Code!"))
 
-@bot.slash_command(guild_only = True, guild_ids = setup.servers)
+
+@bot.slash_command(guild_only=True, guild_ids=setup.servers)
 async def hello(ctx: discord.ApplicationContext):
   await ctx.respond("Hello!")
+
 
 if __name__ == "__main__":
   bot.run(setup.token)
